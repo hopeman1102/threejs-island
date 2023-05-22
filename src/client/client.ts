9e288ec081fc36661b4e1695204b6e0b78ec3364
hopeman1102 <hopeman1102@gmail.com>
2023-05-22 10:09:01 +0900

    update view control

--- src/client/client.ts
+++ src/client/client.ts
@@ -16,15 +16,18 @@ const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000
+    50000
 )
-camera.position.set(0.8, 1.4, 1.0)
+camera.position.set(0, 150, 150)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
+// controls.minPolarAngle = 0
+// controls.maxPolarAngle = 0
+controls.mouseButtons = {LEFT: THREE.MOUSE.RIGHT, MIDDLE: THREE.MOUSE.DOLLY, RIGHT: THREE.MOUSE.ROTATE}
 controls.enableDamping = true
 controls.target.set(0, 1, 0)
 
